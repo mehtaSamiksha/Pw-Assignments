dd27fdc2b35e0998bcd8c3111e32f888f67ac0d3
Samiksha <150701933+mehtaSamiksha@users.noreply.github.com>
2023-11-30 13:32:24 +0530

    Add files via upload

---
+++ OutputVariablesAssignment/Solutions.cpp
@@ -0,0 +1,50 @@
+// Ques: How can you output “Physics”and  “Wallah” in two different lines in C++?
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Physics" << endl << "Wallah" << endl;
+    return 0;
+}
+
+// Ques: Print 10 using 2 positive numbers less than 6  in C++ as output.
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << 5 + 5 << endl;
+}
+
+// Ques: How much space does following data types take ?
+int-2 or 4 bytes
+bool-1 byte
+float-4 bytes
+
+// Ques: What is the output of this program?
+//  void main(){
+//  int a=4;
+//  int b=5;
+//  a++;b--;
+//  cout<<++a<<” “<<b–-;
+//  }
+Compiler will show error
+
+// Ques: WAP to find the circumference of a circle with radius 10 in C++.
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r = 10, pi = 3.1415;
+    cout << 2 * pi * r << endl;
+}
+
+// Ques: How many of these can be a variable name?
+01PWskills - wrong
+
+ _FLOAT - right
+
+ int - wrong
+
+ FLOAT - right
+
+ You will succeed - wrong
